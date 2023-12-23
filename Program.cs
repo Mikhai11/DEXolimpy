2830ba41f18c680b125109466b58bdbcfb62fda1
Mikhai11 <96874130+Mikhai11@users.noreply.github.com>
2023-12-23 11:41:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DexOlympy
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            int[] nums = Array.Empty<int>();
+            try
+            {
+                string[] lines = File.ReadAllLines(@"D:\Olympy\zad1in.txt");
+                string[] elements = lines[0].Split(',');
+                nums = new int[elements.Length];
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    nums[i] = int.Parse(elements[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при чтении файла: {ex.Message}");
+            }
+
+            int k = 0;
+            k = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    k++;
+                }
+            }
+
+            try
+            {
+                File.WriteAllText(@"D:\Olympy\zad1out.txt", k.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при записи в файл: {ex.Message}");
+            }
+
+
+
+        }
+
+    }
+}
